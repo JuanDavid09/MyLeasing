0d7307df7ad18ddcdee71e8ee2da6ae331bf41fa
Juan David Galeano <juand.94@hotmail.com>
2020-02-09 22:00:03 -0500

    Partial Views

--- MyLeasing.Web/Controllers/OwnersController.cs
+++ MyLeasing.Web/Controllers/OwnersController.cs
@@ -247,6 +247,8 @@ namespace MyLeasing.Web.Controllers
                 await _dataContext.SaveChangesAsync();
                 return RedirectToAction($"Details/{model.OwnerId}");
             }
+
+            model.PropertyTypes = _combosHelper.GetComboPropertyTypes();
             return View(model);
         }
 
@@ -394,6 +396,7 @@ namespace MyLeasing.Web.Controllers
                 return RedirectToAction($"{nameof(DetailsProperty)}/{model.PropertyId}");
             }
 
+            model.Lessees = _combosHelper.GetComboLessees();
             return View(model);
         }
 
